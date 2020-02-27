1bdd44a94e5385a844da7e6659c77835a71e4c3e
msm416-laptop <msm416@ic.ac.uk>
2020-02-27 22:28:38 +0000

    fix main in WebServer.java

--- src/main/java/com/develogical/WebServer.java
+++ src/main/java/com/develogical/WebServer.java
@@ -52,8 +52,8 @@ public class WebServer {
   }
 
   public static void main(String[] args) throws Exception {
-    //new WebServer();
-      ActualDBController.getConnection();
+      new WebServer();
+      //ActualDBController.getConnection();
   }
 
 }
