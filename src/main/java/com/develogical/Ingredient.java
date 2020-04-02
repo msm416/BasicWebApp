60e963eee59db6682f0dee0fe9466e034f51ab33
msm416-laptop <msm416@ic.ac.uk>
2020-04-03 02:38:14 +0300

    Proper parsing + percentile test

--- src/main/java/com/develogical/Ingredient.java
+++ src/main/java/com/develogical/Ingredient.java
@@ -1,6 +1,26 @@
 package com.develogical;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Ingredient {
+    final static private List<Pattern> patterns = new ArrayList() {{
+        add(Pattern.compile("\\s*(?<grams>\\d+)g\\s+(?<name>.*)"));
+        // "100g tomatoes"
+        add(Pattern.compile("\\s*(?<nbOfPieces>\\d+)\\s+(?<size>small)\\s*(?<name>.*)"));
+        // "1 small onion"
+        add(Pattern.compile("\\s*(?<nbOfPieces>\\d+)\\s+(?<size>medium)\\s*(?<name>.*)"));
+        // "4 medium salad"
+        add(Pattern.compile("\\s*(?<nbOfPieces>\\d+)\\s+(?<size>large)\\s*(?<name>.*)"));
+        // "3 large kale"
+        add(Pattern.compile("\\s*(?<nbOfPieces>\\d+)\\s+(?<size>tbsp)\\s*(?<name>.*)"));
+        // "2 tbsp olive oil
+        add(Pattern.compile("\\s*(?<nbOfPieces>\\d+)\\s+(?<size>slices of)\\s*(?<name>.*)"));
+        // 3 slices of loaf
+    }};
+
     final String name;
     final double weightInGrams;
 
@@ -10,22 +30,40 @@ public class Ingredient {
     }
 
     public static Ingredient parseIngredient(String ingredientAsStr) {
-        //TODO #1: fix parsing whitespace first, then
-        //TODO #2: implement proper parsing - currently, it works for:
-        // 100g tomatoes, 50g cucumbers, 100g feta cheese, 1 small onion, 2 tbsp olive oil
-        switch (ingredientAsStr) {
-            case "100g tomatoes":
-                return new Ingredient("tomatoes", 100);
-            case " 50g cucumbers":
-                return new Ingredient("cucumbers", 50);
-            case " 100g feta cheese":
-                return new Ingredient("feta cheese", 100);
-            case " 1 small onion":
-                return new Ingredient("small onion", 50);
-            case " 2 tbsp olive oil":
-                return new Ingredient("olive oil", 20);
-            default:
-                return null;
+        for(int i = 0; i < patterns.size(); i++) {
+            Matcher matcher = patterns.get(i).matcher(ingredientAsStr);
+            if(matcher.find()) {
+                //TODO: make this better
+                if(i == 0) {
+                    return new Ingredient(matcher.group("name"), Double.parseDouble(matcher.group("grams")));
+                }
+
+                int gramsPerPiece;
+
+                switch (matcher.group("size")) {
+                    case "small":
+                        gramsPerPiece = 50;
+                        break;
+                    case "medium":
+                        gramsPerPiece = 75;
+                        break;
+                    case "large":
+                        gramsPerPiece = 100;
+                        break;
+                    case "tbsp":
+                        // WE ASSUME FOR SIMPLICITY THAT THE INGREDIENTS HAVE SIMILAR DENSITY TO WATER
+                        // SO IGNORE THE FACT THAT TBSP IS A MEASURE FOR VOLUME
+                        gramsPerPiece = 15;
+                        break;
+                    default:
+                        //case "slices of":
+                        gramsPerPiece = 25;
+                }
+                return new Ingredient(matcher.group("name"),
+                        Integer.parseInt(matcher.group("nbOfPieces")) * gramsPerPiece);
+            }
         }
+
+        return new Ingredient("INVALID: " + ingredientAsStr, 0); // couldn't parse ingredient
     }
 }
--- src/test/java/com/develogical/DBTest.java
+++ src/test/java/com/develogical/DBTest.java
@@ -11,12 +11,21 @@ import java.util.List;
 
 import static org.hamcrest.number.OrderingComparison.lessThan;
 import static org.junit.Assert.assertThat;
+import static utilities.distributions.PerfStatistics.hasPercentile;
 
 public class DBTest {
 
     static final ArrayList mealIngredients = new ArrayList() {{
         add(new Ingredient("ingr1", 100));
         add(new Ingredient("ingr2", 100));
+        add(new Ingredient("ingr3", 100));
+        add(new Ingredient("ingr1", 100));
+        add(new Ingredient("ingr2", 100));
+        add(new Ingredient("ingr3", 100));
+        add(new Ingredient("ingr1", 100));
+        add(new Ingredient("ingr2", 100));
+        add(new Ingredient("ingr3", 100));
+        add(new Ingredient("ingr1", 100));
         add(new Ingredient("ingr3", 100));
     }};
 
@@ -29,22 +38,20 @@ public class DBTest {
 
         final DBController dbController = context.mock(DBController.class);
 
-        context.checking(new Expectations() {{
-            exactly(1).of(dbController).lookupMealIngredients(meal);
-            will(returnValue(mealIngredients));
-            inTime(new NormalDistr(100, 10));
-            exactly(mealIngredients.size()).of(dbController).lookupIngredientNutrition(with(any(Ingredient.class)));
-            will(returnValue(200.0));
-            inTime(new NormalDistr(100, 10));
-        }});
+        context.repeat(1000, () -> {
+                    context.checking(new Expectations() {{
+                        exactly(1).of(dbController).lookupMealIngredients(meal);
+                        will(returnValue(mealIngredients));
+                        inTime(new NormalDistr(100, 10));
+                        exactly(1).of(dbController).lookupIngredientNutrition(with(any(Ingredient.class)));
+                        will(returnValue(200.0));
+                        inTime(new NormalDistr(700, 10));
+                    }});
 
-        long startTime = System.currentTimeMillis();
-        new QueryProcessor(dbController).getNutritionalData(meal);
-        long endTime = System.currentTimeMillis();
-
-        assertThat(context.getSingleVirtualTime(true)
-                        + (endTime - startTime)
-                        - context.getSingleRealTime(),
-                lessThan(800.0));
+                    long startTime = System.currentTimeMillis();
+                    new QueryProcessor(dbController).getNutritionalData(meal);
+                    long endTime = System.currentTimeMillis();
+                });
+        assertThat(context.getMultipleVirtualTimes(), hasPercentile(80, lessThan(8000.0)));
     }
 }
