c03d7fa9fcb41ccfb3187713b7d12971af00f32e
msm416-laptop <msm416@ic.ac.uk>
2020-05-27 02:43:54 +0300

    non-seq calls

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -13,25 +13,29 @@ public class QueryProcessor {
         //long startTime = System.currentTimeMillis();
         List<Ingredient> mealIngredients = dbController.lookupMealIngredients(meal);
         long startTime = System.currentTimeMillis();
+        //long endTime = System.currentTimeMillis();
         //System.out.println("EXECTIME(ms) for lookupMealIngredients() = " + (endTime - startTime));
         //System.out.println("nb of ingredients in meal: " + mealIngredients.size());
 
         double kcals = 0.0;
-//        for(Ingredient ingredient : mealIngredients) {
-//            startTime = System.currentTimeMillis();
-//            kcals += dbController.lookupIngredientNutrition(ingredient);
-//            endTime = System.currentTimeMillis();
-//            //System.out.println("EXECTIME(ms) for lookupIngredientNutrition() = " + (endTime - startTime));
-//        }
+        for(Ingredient ingredient : mealIngredients) {
+            //startTime = System.currentTimeMillis();
+            kcals += dbController.lookupIngredientNutrition(ingredient);
+            //endTime = System.currentTimeMillis();
+            //System.out.println("EXECTIME(ms) for lookupIngredientNutrition() = " + (endTime - startTime));
+        }
 
-        double kcals_par = mealIngredients.parallelStream()
-                .map(dbController::lookupIngredientNutrition)
-                .reduce(0d, (a,b) -> a + b);
         long endTime = System.currentTimeMillis();
-        System.out.println("EXECTIME(ms) for lookupIngredientNutritionCombinedParallel() = " + (endTime - startTime));
+        System.out.println("EXECTIME(ms) for lookupIngredientNutritionCombinedSequential() = " + (endTime - startTime));
+
+//        double kcals_par = mealIngredients.parallelStream()
+//                .map(dbController::lookupIngredientNutrition)
+//                .reduce(0d, (a,b) -> a + b);
+//        long endTime = System.currentTimeMillis();
+//        System.out.println("EXECTIME(ms) for lookupIngredientNutritionCombinedParallel() = " + (endTime - startTime));
 
         //System.out.println("nb of kcals in meal: "  + kcals);
-        return "nb of kcals in meal: "  + kcals_par;
+        return "nb of kcals in meal: "  + kcals;
     }
 
     public String suggestAMeal(int dishComplexity) {
